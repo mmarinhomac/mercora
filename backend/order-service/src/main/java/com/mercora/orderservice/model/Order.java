556d4f789b850d664e46420a8262aa806c5c7c35
Matheus Marinho <mmarinhomac@gmail.com>
2026-02-16 08:13:04 -0300

    order model

--- backend/order-service/src/main/java/com/mercora/orderservice/model/Order.java
+++ backend/order-service/src/main/java/com/mercora/orderservice/model/Order.java
@@ -2,53 +2,56 @@ package com.mercora.orderservice.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @Entity
 @Table(name = "orders")
+@SuppressWarnings("JpaDataSourceORMInspection")
 public class Order {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private UUID id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private UUID id;
 
-    @NotNull
-    private String customerId;
+  @NotNull
+  private String customerId;
 
-    @NotNull
-    private String status;
+  @NotNull
+  private String status;
 
-    @NotNull
-    private Double total;
+  @NotNull
+  private BigDecimal total;
 
-    public UUID getId() {
-        return id;
-    }
+  public UUID getId() {
+    return id;
+  }
 
-    public void setId(UUID id) {
-        this.id = id;
-    }
+  public void setId(UUID id) {
+    this.id = id;
+  }
 
-    public String getCustomerId() {
-        return customerId;
-    }
+  public String getCustomerId() {
+    return customerId;
+  }
 
-    public void setCustomerId(String customerId) {
-        this.customerId = customerId;
-    }
+  public void setCustomerId(String customerId) {
+    this.customerId = customerId;
+  }
 
-    public String getStatus() {
-        return status;
-    }
+  public String getStatus() {
+    return status;
+  }
 
-    public void setStatus(String status) {
-        this.status = status;
-    }
+  public void setStatus(String status) {
+    this.status = status;
+  }
 
-    public Double getTotal() {
-        return total;
-    }
+  public BigDecimal getTotal() {
+    return total;
+  }
 
-    public void setTotal(Double total) {
-        this.total = total;
-    }
+  public void setTotal(BigDecimal total) {
+    this.total = total;
+  }
 }
